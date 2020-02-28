275cdc21ed711cc9165fedca7681c46c6396b13f
Dav1do <dav1do@@users.noreply.github.com>
2020-02-27 19:13:14 -0700

    refactor to use lib.rs and call functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,114 @@
+use regex::Regex;
+use slack_api as slack;
+use std::collections::HashMap;
+
+#[macro_use]
+extern crate lazy_static;
+
+pub fn join_channel_if_necessary<R>(
+    slack_client: &R,
+    slack_api_token: &str,
+    channel: &slack::Channel,
+    user: &slack::User,
+)
+//-> Result<slack::channels::JoinResponse, slack::channels::JoinError<R::Error>>
+// TODO how do we encapsulate both the idea "should we join" and "did we join"?
+//if that's overloaded, should it be 2 functions or just in line?
+where
+    R: slack::requests::SlackWebRequestSender,
+{
+    let channel_name = &channel.name.clone().unwrap();
+    if !channel
+        .members
+        .as_ref()
+        .unwrap()
+        .iter()
+        .any(|m| m == user.id.as_ref().unwrap())
+    {
+        // println!("joining {}", &channel_name);
+        let request = slack::channels::JoinRequest {
+            name: &channel_name,
+            validate: Some(true),
+        };
+        let response = slack::channels::join(slack_client, slack_api_token, &request);
+        let result = response.expect("unable to join channel"); //TODO map and print error
+    } else {
+        // println!("already in {}", &channel_name);
+        //this doesn't error if we try to join again so maybe we just always join?
+    }
+}
+
+pub fn process_channel_history<R>(client: &R, api_token: &str, channel: &slack::Channel)
+where
+    R: slack::requests::SlackWebRequestSender,
+{
+    let request = slack::channels::HistoryRequest {
+        channel: &channel.id.clone().unwrap(),
+        inclusive: Some(true),
+        latest: None,
+        oldest: None,
+        count: Some(200),
+        unreads: Some(true),
+    };
+    let response = slack::channels::history(client, &api_token, &request);
+    if let Ok(response) = response {
+        // println!("{:?}", response.messages.clone().unwrap());
+        process_messages(&response.messages.unwrap());
+    } else {
+        println!("{:?}", response);
+    }
+}
+
+fn process_messages(messages: &[slack::Message]) {
+    //need a mutable hash of user: [<emoji : count>] that we can add to efficiently
+    for message in messages {
+        //TODO if let here, but how exactly?
+        match message {
+            slack::Message::Standard(m) => {
+                let parsed_messages = get_emoji_counts_from_message(m);
+                println!("{:?}", parsed_messages);
+                //TODO add a 
+            }
+            _ => (),
+        }
+    }
+}
+// TODO turn this into something that takes a reference to the current HashMap of all users and just adds for ours?
+// also, move this stuff to lib.rs once it's working
+fn get_emoji_counts_from_message(
+    message: &slack::MessageStandard,
+) -> HashMap<String, HashMap<String, u32>> {
+    let m = message.text.clone().unwrap();
+    let mut u = HashMap::new();
+    let x = extract_emojis(&m);
+    u.insert(message.user.clone().unwrap(), x);
+    u
+}
+
+fn extract_emojis(text: &str) -> HashMap<String, u32> {
+    lazy_static! {
+        //this finds :: as a valid emoji :/
+        static ref EMOJI_REGEX: Regex = Regex::new(r":([a-z1-9-_]*?):").unwrap();
+    }
+    let mut res = HashMap::new();
+
+    let emojis: Vec<&str> = EMOJI_REGEX
+        .find_iter(text)
+        .map(|mat| mat.as_str())
+        .collect();
+
+    for emoji in emojis {
+        let e = emoji.to_string();
+        if let Some(count) = res.get_mut(&e) {
+            *count += 1;
+        } else {
+            res.insert(e, 1);
+        }
+    }
+
+    res
+}
+
 // use std::sync::mpsc;
 // use std::thread;
 // use std::time::Duration;
@@ -73,7 +184,6 @@
 //     }
 //     channels_result
 // }
-
 
 // use std::sync::mpsc;
 // use std::thread;
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,6 @@
-use regex::Regex;
+use emoji_index;
 use slack_api as slack;
-use std::collections::HashMap;
 use std::env;
-
-#[macro_use]
-extern crate lazy_static;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     const BOT_NAME: &str = "emoji_analyzer";
@@ -29,103 +25,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
     }
     //could do this in previous loop, but passing the user out seems clunky (probably missing something)
-    let our_bot_user = &ignored_users
+    let our_bot_user = ignored_users
         .iter()
         .find(|u| u.real_name == Some(BOT_NAME.to_string()))
         .expect("unable to find our bot user. exiting");
-    //get the member info for current user/app
-    //get all channels
-    //join channels if we aren't in them
-    for channel in &channels {
-        //it seems like this should be the array rather than needing to go in another for loop here
-        // refactor into join_channels method
-        for members in channel.members.iter() {
-            //TODO not reference with iter()
-            if !members
-                .iter()
-                .any(|m| *m == our_bot_user.id.clone().unwrap().to_string())
-            {
-                // TODO update them!
-                println!("updating {:?}", &channel.name.clone().unwrap());
-                let request = slack::channels::JoinRequest {
-                    name: &*channel.name.clone().unwrap(),
-                    validate: Some(true),
-                };
-                let response = slack::channels::join(&client, &token, &request);
-                let result = response.expect("unable to join channel"); //TODO map and print error
-            }
-        }
-        //for each channel, get messages (paginating back)
-        let request = slack::channels::HistoryRequest {
-            channel: &channel.id.clone().unwrap(),
-            inclusive: Some(true),
-            latest: None,
-            oldest: None,
-            count: Some(200),
-            unreads: Some(true),
-        };
-        let response = slack::channels::history(&client, &token, &request);
-        if let Ok(response) = response {
-            // println!("{:?}", response.messages.clone().unwrap());
-            process_messages(&response.messages.unwrap());
 
-            if response.has_more == Some(true) {
-                // println!("more messages {:?}", &response);
-            }
-        } else {
-            println!("{:?}", response);
-        }
-        //for each message, find emojis used by member
-        //for each response, find emojis used by member
+    //this will have the public join channels if we aren't in them
+    for channel in &channels {
+        emoji_index::join_channel_if_necessary(&client, &token, channel, our_bot_user);
+        emoji_index::process_channel_history(&client, &token, channel);
     }
+    
     Ok(())
 }
 // {user : {emoji : count}}
 //
-
-fn process_messages(messages: &[slack::Message]) {
-    // let mut hash = HashMap::new();
-    // let mut emojimap = HashMap::new();
-    // emojimap.insert("grumpycat", 1);
-    // hash.insert("user name", emojimap);
-    for message in messages {
-        match message {
-            slack::Message::Standard(m) => println!("{:?}", get_emoji_counts_from_message(m)),
-            _ => println!("other message: {:?}", message),
-        }
-    }
-}
-
-// TODO turn this into something that takes a reference to the current HashMap of all users and just adds for ours?
-// also, move this stuff to lib.rs once it's working
-fn get_emoji_counts_from_message(message: &slack::MessageStandard) -> HashMap<String, HashMap<String, i32>> {
-    let m = message.text.clone().unwrap();
-    let mut u = HashMap::new();
-    let x = extract_emojis(&m);
-    u.insert(message.user.clone().unwrap(), x);
-    u
-}
-
-fn extract_emojis(text: &str) -> HashMap<String, i32> {
-    lazy_static! {
-        //this finds :: as a valid emoji :/
-        static ref EMOJI_REGEX: Regex = Regex::new(r":([a-z1-9-_]*?):").unwrap();
-    }
-    let mut res = HashMap::new();
-
-    let emojis: Vec<&str> = EMOJI_REGEX
-        .find_iter(text)
-        .map(|mat| mat.as_str())
-        .collect();
-
-    for emoji in emojis {
-        let e = emoji.to_string();
-        if let Some(count) = res.get_mut(&e) {
-            *count += 1;
-        } else {
-            res.insert(e, 1);
-        }
-    }
-
-    res
-}
